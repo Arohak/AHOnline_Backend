820dceaf064cf15d5ae82480b625bcbde27cad61
Ara Hakobyan <araha@Administrators-Mac-mini-2.local>
2016-07-11 16:20:27 +0400

    add api for home

---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,17 @@
+class API::V1::RestaurantsController < ApplicationController
+
+  def home
+    news =  Restaurant.where(new: true).limit(5)
+    rates =  Restaurant.where(rate: 4).limit(5)
+    opens =  Restaurant.where(status: 'open').limit(5)
+
+    response = { data: {news: news,
+                        rates: rates,
+                        opens: opens},
+
+                 result: {status: "SUCCESS",
+                          message: ""} }
+
+    render json: response
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
 
   get '/parse', to: 'getdata#parse'
 
+  namespace :api do
+    namespace :v1 do
+      get 'restaurants/home', to: 'restaurants#home'
+
+      # resources :profiles do
+        # get :me, on: :collection
+      # end
+
+    end
+  end
+
 end
